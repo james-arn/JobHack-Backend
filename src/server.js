82d6473e9215b2a99bc7eb42d0cf81ce7c75ea22
James Arnold <j1mesu@hotmail.co.uk>
2021-11-28 19:31:43 +0000

    hosting

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
-require("./db/connection.js");
+require("../src/db/connection");
 const express = require("express");
 const cors = require("cors");
-const userRouter = require("./user/user.controllers.js");
-const app = express();
-const port = process.env.PORT || 5000;
+const userRouter = require("./user/user.routes");
+const app = express(); // have to assign to const
+const port = process.env.PORT || 5000; //have to assign to port.
 
+//need 3 things:
 app.use(express.json());
-app.use(cors());
+//recieves request as JOSN, converts to JS object, then when sending out, it converts it to JSON. This increases speed massively.
+//it's a string so super small fiel szie and rapid.
+app.use(cors()); //cross origin resource sharing enables
 app.use(userRouter);
+// below is anoyminous function is a controller. hit health in point. good to check and a test.
+app.get("/health", (req, res) => {
+  res.send({ message: "server's up" });
+});
 
-app.get("/health", (req, res) => res.send("Alive!"));
-
+//below enables app to listenon lport, console logs it's listening on port. must be at bottom.
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`); //ensure backticks.
 });
