2fb19b04c72710096b0abdc3c74a3f421e37a64e
James Arnold <j1mesu@hotmail.co.uk>
2021-11-29 10:43:07 +0000

    job model complete

---
+++ src/job/job.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: Number,
+  },
+});
+
+const Job = mongoose.model("Job", jobSchema);
+
+module.exports = Job;
